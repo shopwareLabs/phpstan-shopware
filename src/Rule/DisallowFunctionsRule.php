c9362ac9aad83f89b633b6ec884c47fb95597ddd
tinect <s.koenig@tinect.de>
2024-10-04 13:11:35 +0200

    feat: add disallowed functions

---
+++ src/Rule/DisallowFunctionsRule.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\PhpStan\Rule;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleError;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements Rule<FuncCall>
+ *
+ * @internal
+ */
+class DisallowFunctionsRule implements Rule
+{
+
+    private const NOT_ALLOWED_FUNCTIONS = [
+        'var_dump',
+        'exit',
+        'die',
+    ];
+
+    public function getNodeType(): string
+    {
+        return FuncCall::class;
+    }
+
+    /**
+     * @return array<array-key, RuleError|string>
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node instanceof FuncCall) {
+            return [];
+        }
+
+        if (!$node->name instanceof Node\Name) {
+            return [];
+        }
+
+        $name = $node->name->toString();
+
+        if (\in_array($name, self::NOT_ALLOWED_FUNCTIONS, true)) {
+            return [
+                RuleErrorBuilder::message(\sprintf('Do not use %s function in code.', $name))->build(),
+            ];
+        }
+
+        return [];
+    }
+}
